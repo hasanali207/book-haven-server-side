4750a8491837e7f094247f55482c136eb02ad6ef
hasanali207 <hasanali142228@gmail.com>
2024-05-12 20:02:29 +0600

    added Details route

--- index.js
+++ index.js
@@ -1,19 +1,29 @@
-const express = require('express')
-const cors = require('cors')
+const express = require('express');
+const cors = require('cors');
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser')
+require('dotenv').config();
 
-const app = express()
-const port = process.env.PORT || 5000
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wdiwjgo.mongodb.net`;
 
+const app = express();
+const port = process.env.PORT || 5000;
 
-app.use(express.json())
-app.use(cors())
-require('dotenv').config()
+app.use(express.json());
+app.use(cookieParser())
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "http://localhost:5174",
+      "https://cardoctor-bd.web.app",
+      "https://cardoctor-bd.firebaseapp.com",
+    ],
+    credentials: true,
+  })
+);
 
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wdiwjgo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -24,48 +34,72 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-  
-    // await client.connect();
-   
+    await client.connect();
     const database = client.db("bookItem");
     const itemCollection = database.collection("Items");
 
-// jwt emplement 
-
-        
-    app.post('/items', async(req, res) =>{
-        const newItem = req.body
-        console.log(newItem)
-        const result = await itemCollection.insertOne(newItem)
-        res.send(result)
+    // JWT implementation
+   //creating Token
+   app.post("/jwt",  async (req, res) => {
+    const user = req.body;
+    console.log("user for token", user);
+    const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
+  
+    res.cookie('token', token, {
+      httpOnly:true,
+      secure: process.env.NODE_ENV === 'production',
+      sameSite: process.env.NODE_ENV === 'production'?'none':'strict' 
     })
-    
-    app.get('/items', async(req, res) =>{
-        const cursor = itemCollection.find()
-        const result = await cursor.toArray()
-        res.send(result)
+    .send({success:true})
+  });
+
+  app.get("/logout", async (req, res) => {
+       
+      res.clearCookie("token", {
+      httpOnly:true,
+      secure: process.env.NODE_ENV === 'production',
+      sameSite: process.env.NODE_ENV === 'production'?'none':'strict' ,
+       maxAge: 0 })
+      .send({ success: true });
+  });
+
+    app.post('/items', async (req, res) => {
+      
+        const newItem = req.body;
+        const result = await itemCollection.insertOne(newItem);
+        res.send(result);
+    }
+    );
+
+    app.get('/items', async (req, res) => {
+     
+        const cursor = itemCollection.find();
+        const result = await cursor.toArray();
+        res.send(result);
+      
+    });
+
+
+    app.get('/items/:id', async(req, res)=>{
+      const id = req.params.id
+      const query = {_id : new ObjectId(id)}
+      const result = await itemCollection.findOne(query)
+      res.send(result)
     })
 
-
-    
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
-    
     // await client.close();
   }
 }
+
 run().catch(console.dir);
 
+app.get('/', async (req, res) => {
+  res.send('Start Book Haven Server');
+});
 
-
-
-
-app.get('/', async(req, res) =>{
-    res.send('Start Book Haven Server')
-})
-
-app.listen(port, () =>{
-    console.log('Server Running');
-    
-})
+app.listen(port, () => {
+  console.log(`Server Running ${port}`);
+});
