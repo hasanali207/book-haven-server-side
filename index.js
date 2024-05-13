5875c1664624ce6f5ef1b3e83815a56250890fae
hasanali207 <hasanali142228@gmail.com>
2024-05-13 13:23:15 +0600

    added data for borrow

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-const express = require('express');
-const cors = require('cors');
-const jwt = require('jsonwebtoken');
-const cookieParser = require('cookie-parser')
-require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
+require("dotenv").config();
 
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wdiwjgo.mongodb.net`;
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
-app.use(cookieParser())
+app.use(cookieParser());
 app.use(
   cors({
     origin: [
@@ -29,7 +29,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -44,96 +44,104 @@ async function run() {
       console.log("user for token", user);
       const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
 
-      res.cookie('token', token, {
-        httpOnly:true,
-        secure: process.env.NODE_ENV === 'production',
-        sameSite: process.env.NODE_ENV === 'production'?'none':'strict' 
-      })
-      .send({success:true})
+      res
+        .cookie("token", token, {
+          httpOnly: true,
+          secure: process.env.NODE_ENV === "production",
+          sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+        })
+        .send({ success: true });
     });
 
     app.get("/logout", async (req, res) => {
-      res.clearCookie("token", {
-        httpOnly:true,
-        secure: process.env.NODE_ENV === 'production',
-        sameSite: process.env.NODE_ENV === 'production'?'none':'strict' ,
-        maxAge: 0 
-      }).send({ success: true });
+      res
+        .clearCookie("token", {
+          httpOnly: true,
+          secure: process.env.NODE_ENV === "production",
+          sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+          maxAge: 0,
+        })
+        .send({ success: true });
     });
 
-    app.post('/items', async (req, res) => {
-      
-        const newItem = req.body;
-        const result = await itemCollection.insertOne(newItem);
-        res.send(result);
-            
-    });
- 
-  
-    app.get('/items', async (req, res) => {
-    
-        const cursor = itemCollection.find();
-        const result = await cursor.toArray();
-        res.send(result);
-      
-    });
-    app.post('/borrow', async (req, res) => {
-      
+    app.post("/items", async (req, res) => {
       const newItem = req.body;
-      const result = await bookCollection.insertOne(newItem);
+      const result = await itemCollection.insertOne(newItem);
       res.send(result);
-          
-  });
-    app.get('/borrow', async (req, res) => {
-    
-        const cursor = bookCollection.find();
-        const result = await cursor.toArray();
-        res.send(result);
-      
     });
-    app.get('/borrow/:email', async (req, res) => {
+
+    app.get("/items", async (req, res) => {
+      const cursor = itemCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+   
+
+    app.get("/items/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await itemCollection.findOne(query);
+      res.send(result);
+    });
+
+    app.get("/data/:category", async (req, res) => {
+      const category = req.params.category;
+      const query = { category: category };
+      const result = await itemCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    app.get("/books/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const book = await itemCollection.findOne(query);
+      res.send(book);
+    });
+
+    app.get("/borrow/book/:email", async (req, res) => {
       const email = req.params.email;
       const query = { user_email: email };
       const result = await bookCollection.find(query).toArray();
       res.send(result);
-      
     });
 
 
-    app.get('/items/:id', async (req, res) => {
-     
-        const id = req.params.id;
-        const query = { _id: new ObjectId(id) };
-        const result = await itemCollection.findOne(query);
-        res.send(result);
-     
-    });
-
-    app.get('/data/:category', async (req, res) => {
-        const category = req.params.category;
-        const query = { category: category };
-        const result = await itemCollection.find(query).toArray();
-        res.send(result);
-      
+    app.post("/borrow", async (req, res) => {
+      const newItem = req.body;
+      const result = await bookCollection.insertOne(newItem);
+      res.send(result);
     });
 
 
-    app.get('/books/:id', async (req, res) => {
-      const id = req.params.id;
-      const query = { _id: new ObjectId(id) };
-      const book = await itemCollection.findOne(query);
-      res.send(book)
+    //   app.post('/borrow/:id', async (req, res) => {
+    //     const itemId = req.params.id
+    //     const newItem = req.body;
+    //     const update = await itemCollection.updateOne(
+    //       { _id:  ObjectId(itemId) },
+    //       { $inc: { quantity: 1 } }
+    //   );
+
+    //     const result = await bookCollection.insertOne(newItem);
+    //     res.send(result, update);
+
+    // });
+
+    app.get("/borrow", async (req, res) => {
+      const cursor = bookCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+      console.log("this is work")
     });
 
-    
 
-    
-  
 
 
 
     // await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } catch (error) {
     console.error("Error connecting to MongoDB:", error);
   } finally {
@@ -143,11 +151,10 @@ async function run() {
 
 run().catch(console.dir);
 
-app.get('/', async(req, res) =>{
-    res.send('Start Server ')
-})
+app.get("/", async (req, res) => {
+  res.send("Start Server ");
+});
 
-app.listen(port, () =>{
-    console.log(`Server is Runnig ${port}` );
-    
-})
+app.listen(port, () => {
+  console.log(`Server is Runnig ${port}`);
+});
