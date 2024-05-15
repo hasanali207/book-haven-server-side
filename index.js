c06ce732bdf2dc05d097c1b70be489991001d72c
hasanali207 <hasanali142228@gmail.com>
2024-05-15 19:08:29 +0600

    added token

--- index.js
+++ index.js
@@ -12,13 +12,29 @@ const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cookieParser());
+
+const verfyToken = async (req, res, next) => {
+  const token = req.cookies?.token;
+  console.log(token);
+  if (!token) {
+    res.status(401).send({ message: "Not authorized" });
+  }
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, async (error, decoded) => {
+    if (error) {
+      return res.status(401).send({ message: error.message });
+    }
+    req.user = decoded;
+    next();
+  });
+};
+
 app.use(
   cors({
     origin: [
       "http://localhost:5173",
       "http://localhost:5174",
-      "https://cardoctor-bd.web.app",
-      "https://cardoctor-bd.firebaseapp.com",
+      "https://book-haven-six.vercel.app",
+      "book-haven-bba9e.firebaseapp.com",
     ],
     credentials: true,
   })
@@ -32,9 +48,15 @@ const client = new MongoClient(uri, {
   },
 });
 
+
+const cookieOptions = {
+  httpOnly: true,
+  secure: process.env.NODE_ENV === "production",
+  sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+};
 async function run() {
   try {
-    await client.connect();
+    // await client.connect();
     const database = client.db("bookItem");
     const itemCollection = database.collection("Items");
     const bookCollection = database.collection("books");
@@ -43,22 +65,15 @@ async function run() {
       const user = req.body;
       console.log("user for token", user);
       const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
-
+      console.log(token);
       res
-        .cookie("token", token, {
-          httpOnly: true,
-          secure: process.env.NODE_ENV === "production",
-          sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
-        })
+        .cookie("token", token, cookieOptions)
         .send({ success: true });
     });
 
     app.get("/logout", async (req, res) => {
       res
-        .clearCookie("token", {
-          httpOnly: true,
-          secure: process.env.NODE_ENV === "production",
-          sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+        .clearCookie("token", {...cookieOptions, 
           maxAge: 0,
         })
         .send({ success: true });
@@ -70,11 +85,13 @@ async function run() {
       res.send(result);
     });
 
-    app.get("/items", async (req, res) => {
+
+    app.get("/items",  async (req, res) => {
       const cursor = itemCollection.find();
       const result = await cursor.toArray();
       res.send(result);
     });
+
 
     app.get("/items/:id", async (req, res) => {
       const id = req.params.id;
@@ -93,7 +110,7 @@ async function run() {
     app.put("/borrowedBook/:id", async (req, res) => {
       const borrowedBookId = req.params.id;
       const updateBorrewdBook = await itemCollection.updateOne(
-        { _id: new ObjectId(borrowedBookId) }, 
+        { _id: new ObjectId(borrowedBookId) },
         { $inc: { quantity: -1 } }
       );
 
@@ -102,16 +119,15 @@ async function run() {
 
     app.put("/getreturn/:id", async (req, res) => {
       const returnBookId = req.params.id;
-      
-       const updateBorrowedBook = await itemCollection.findOneAndUpdate(
-      { _id: new ObjectId(returnBookId) },
-      { $inc: { quantity: 1 } },
-      { returnOriginal: false } 
-    );
+
+      const updateBorrowedBook = await itemCollection.findOneAndUpdate(
+        { _id: new ObjectId(returnBookId) },
+        { $inc: { quantity: 1 } },
+        { returnOriginal: false }
+      );
       res.send(updateBorrowedBook);
       console.log(updateBorrowedBook);
     });
-
 
     app.post("/borrowed", async (req, res) => {
       const newItem = req.body;
@@ -135,36 +151,31 @@ async function run() {
       res.send(result);
     });
 
-     // update
-     app.get('/items/update/:id', async(req, res)=>{
-      const id = req.params.id
-      const query = {_id : new ObjectId(id)}
-      const result = await itemCollection.findOne(query)
-      res.send(result)
-    })
-     app.put('/updateItem/:id', async(req, res)=>{
-      const id = req.params.id
-      const item = req.body
-      const query = {_id : new ObjectId(id)}
-     const data = {
-        $set:{
+    // update
+    app.get("/items/update/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await itemCollection.findOne(query);
+      res.send(result);
+    });
+    app.put("/updateItem/:id", async (req, res) => {
+      const id = req.params.id;
+      const item = req.body;
+      const query = { _id: new ObjectId(id) };
+      const data = {
+        $set: {
           image: item.image,
           name: item.name,
-          // quantity: item.quantity,         
+          // quantity: item.quantity,
           rating: item.rating,
           description: item.description,
           category: item.category,
-          
-        } 
-      }
-      const result = await itemCollection.updateOne(query, data )
+        },
+      };
+      const result = await itemCollection.updateOne(query, data);
       console.log(result);
-      res.send(result)
-      
-    })
-
-
-
+      res.send(result);
+    });
 
     // await client.db("admin").command({ ping: 1 });
     console.log(
